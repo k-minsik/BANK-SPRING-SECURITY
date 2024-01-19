9eb35cd19934d9bcb8fd1d164e07c5285ca67cce
k-minsik <smin58810@gmail.com>
2024-01-19 18:17:40 +0900

    feat: Connect aws cloud database

---
+++ src/main/java/kminsik/bankbackend/model/Customer.java
@@ -0,0 +1,31 @@
+package kminsik.bankbackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Customer {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String email;
+
+    private String pwd;
+
+    private String role;
+
+    public Customer(String email, String pwd, String role) {
+        this.email = email;
+        this.pwd = pwd;
+        this.role = role;
+    }
+}
